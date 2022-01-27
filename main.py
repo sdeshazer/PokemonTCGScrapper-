5e27a8e854902ee8b309cbce29cb23b3859e2796
sdeshazer <sdeshazer92@gmail.com>
2022-01-26 19:35:28 -0800

    saves names and prices to cvs file

--- main.py
+++ main.py
@@ -17,39 +17,35 @@ def scaper_test():
         page = urllib.request.urlopen(pg)
         soup = BeautifulSoup(page, 'html.parser')
         attr = soup.find('table', class_="priceGuideTable tablesorter")
-        data = get_Cards(attr)
+        get_Cards(attr)
 
 
 def get_Cards(collection):
-    card_collection_odd =  collection.findAll('tr', class_="odd")
-    card_collection_even = collection.findAll('tr', class_="even")
-    card_collection = card_collection_even + card_collection_odd
-    for card in card_collection:
-        card_name = card.find('div', class_="productDetail")
-        card_price = card.find('td', class_="marketPrice")
-        print(card_name.text.strip())
-        print(card_price.text.strip())
-    print("total:")
-    print(len(card_collection))
-    return card_collection
-
-    #data.append((name_of_card, price_of_card.text.strip()))
-
-
-
-def write_data():
     with open(csv_path, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        print(len(data))
-        for name_of_card, price in data:
-            print(name_of_card)
-            print(price)
-            writer.writerow([name_of_card, price])
+        card_collection_odd = collection.findAll('tr', class_="odd")
+        card_collection_even = collection.findAll('tr', class_="even")
+        card_collection = card_collection_even + card_collection_odd
+        for card in card_collection:
+            card_name = card.find('div', class_="productDetail")
+            card_price = card.find('td', class_="marketPrice")
+            print(card_name.text.strip())
+            print(card_price.text.strip())
+            write_data(csv_file, card_name.text.strip(), card_price.text.strip())
+        print("total:")
+        print(len(card_collection))
+
+
+    # data.append((name_of_card, price_of_card.text.strip()))
+
+
+def write_data(csv_file, card_name, card_price):
+    writer = csv.writer(csv_file)
+    writer.writerow([card_name, card_price])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     scaper_test()
- #   parse_Cards(data)
-   # write_data()
-   #print(data)
+#   parse_Cards(data)
+# write_data()
+# print(data)
